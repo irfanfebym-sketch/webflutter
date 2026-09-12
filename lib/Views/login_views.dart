4363064298980ab796c458644f7c3a86cc10da2b
irfan <irfanfebym@gmail.com>
2026-09-12 13:22:02 +0700

    kerangka login

--- lib/Views/login_views.dart
+++ lib/Views/login_views.dart
@@ -1,32 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
-import 'lib/style.dart';
-import 'lib/Controller/simple_ui.dart';
+import 'package:flutter_application_1/Controller/simple_ui.dart';
+import 'package:flutter_application_1/style.dart';
 
-class LoginView extends StatelessWidget {
-  final SimpleUIController simpleUIController = Get.put(SimpleUIController());
+class LoginView extends StatefulWidget {
+const LoginView({Key? key}) : super(key: key);
 
   @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  TextEditingController namaController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    namaController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+@override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Obx(() {
-          if (simpleUIController.isLoading.value) {
-            return Lottie.asset('assets/loading.json', width: 200, height: 200);
-          } else {
-            return ElevatedButton(
-              onPressed: () {
-                simpleUIController.setLoading(true);
-                Future.delayed(Duration(seconds: 2), () {
-                  simpleUIController.setLoading(false);
-                });
-              },
-              child: Text('Login'),
-            );
-          }
-        }),
+    var size = MediaQuery.of(context).size;
+    SimpleUIController simpleUIController = Get.put(SimpleUIController());
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: false,
+      body: LayoutBuilder (
+            builder : (context, constraints) {
+            if (constraints.maxWidth > 600) {
+              return _buildLargeScreen(size, simpleUIController);
+            } else {
+              return _buildSmallScreen(size, simpleUIController);
+            }
+          },
+        ),
       ),
     );
+  }
+
+  Widget _buildLargeScreen(Size size, SimpleUIController controller) {
+    return const Center(child: Text('Layar Besar'));
+  }
+
+  Widget _buildSmallScreen(Size size, SimpleUIController controller) {
+    return const Center(child: Text('Layar Kecil'));
   }
 }
--- lib/style.dart
+++ lib/style.dart
@@ -1,3 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+TextStyle kmyLoginTextStyle(Size size) => GoogleFonts.ubuntu(
+  fontSize: size.height * 0.060,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle kmyTitleTextStyle(Size size) => GoogleFonts.ubuntu(
+  fontSize: size.height * 0.030,
+);
+
+TextStyle kLoginTermsAndPrivacyStyle(Size size) =>
+    GoogleFonts.ubuntu(fontSize: 15, color: Colors.grey, height: 1.5);
+
+TextStyle kHaveAnAccountStyle(Size size) =>
+    GoogleFonts.ubuntu(fontSize: size.height * 0.022, color: Colors.black);
+
+TextStyle kLoginOrSignUpTextStyle(
+  Size size,
+) =>
+    GoogleFonts.ubuntu(
+      fontSize: size.height * 0.022,
+      fontWeight: FontWeight.w500,
+      color: Colors.deepPurpleAccent,
+    );
+
+TextStyle kTextFormFieldStyle() => const TextStyle(color: Colors.black);
