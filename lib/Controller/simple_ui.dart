b7f43deecb450f9b35cce4a65647daeaa9ac2493
irfan <irfanfebym@gmail.com>
2026-09-12 22:43:54 +0700

    Login page

--- lib/Controller/simple_ui.dart
+++ lib/Controller/simple_ui.dart
@@ -2,8 +2,13 @@ import 'package:get/get.dart';
 
 class SimpleUIController extends GetxController {
   var isLoading = false.obs;
+  var isObscure = true.obs;
 
   void setLoading(bool value) {
     isLoading.value = value;
+  }
+
+  void isObscureActive() {
+    isObscure.value = !isObscure.value;
   }
 }
--- lib/Views/login_views.dart
+++ lib/Views/login_views.dart
@@ -5,7 +5,7 @@ import 'package:flutter_application_1/Controller/simple_ui.dart';
 import 'package:flutter_application_1/style.dart';
 
 class LoginView extends StatefulWidget {
-const LoginView({Key? key}) : super(key: key);
+  const LoginView({Key? key}) : super(key: key);
 
   @override
   State<LoginView> createState() => _LoginViewState();
@@ -26,17 +26,17 @@ class _LoginViewState extends State<LoginView> {
     super.dispose();
   }
 
-@override
+  @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     SimpleUIController simpleUIController = Get.put(SimpleUIController());
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: Scaffold(
-      backgroundColor: Colors.white,
-      resizeToAvoidBottomInset: false,
-      body: LayoutBuilder (
-            builder : (context, constraints) {
+        backgroundColor: Colors.white,
+        resizeToAvoidBottomInset: false,
+        body: LayoutBuilder(
+          builder: (context, constraints) {
             if (constraints.maxWidth > 600) {
               return _buildLargeScreen(size, simpleUIController);
             } else {
@@ -48,11 +48,210 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 
-  Widget _buildLargeScreen(Size size, SimpleUIController controller) {
-    return const Center(child: Text('Layar Besar'));
+  // Large Screen
+  Widget _buildLargeScreen(
+    Size size,
+    SimpleUIController simpleUIController,
+  ) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 4,
+          child: RotatedBox(
+            quarterTurns: 3,
+            child: Lottie.asset(
+              'assets/coin.json',
+              height: size.height * 0.3,
+              width: double.infinity,
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+        SizedBox(width: size.width * 0.06),
+        Expanded(
+          flex: 5,
+          child: _buildMainBody(size, simpleUIController),
+        ),
+      ],
+    );
   }
 
-  Widget _buildSmallScreen(Size size, SimpleUIController controller) {
-    return const Center(child: Text('Layar Kecil'));
+  // Small Screen
+  Widget _buildSmallScreen(
+    Size size,
+    SimpleUIController simpleUIController,
+  ) {
+    return Center(
+      child: _buildMainBody(size, simpleUIController),
+    );
+  }
+
+  /// Main Body
+  Widget _buildMainBody(
+    Size size,
+    SimpleUIController simpleUIController,
+  ) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: size.width > 600
+          ? MainAxisAlignment.center
+          : MainAxisAlignment.start,
+      children: [
+        size.width > 600
+            ? Container()
+            : Lottie.asset(
+                'assets/wave.json',
+                height: size.height * 0.2,
+                width: size.width,
+                fit: BoxFit.fill,
+              ),
+        SizedBox(height: size.height * 0.03),
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: Text(
+            'Login',
+            style: kmyLoginTextStyle(size),
+          ),
+        ),
+        const SizedBox(height: 10),
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: Text(
+            'Welcome Back Catchy',
+            style: kmyTitleTextStyle(size),
+          ),
+        ),
+        SizedBox(height: size.height * 0.03),
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 20),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                /// Username or Gmail
+                TextFormField(
+                  style: kTextFormFieldStyle(),
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.person),
+                    hintText: 'Username or Gmail',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                  ),
+                  controller: namaController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter username';
+                    } else if (value.length < 4) {
+                      return 'at least enter 4 characters';
+                    } else if (value.length > 13) {
+                      return 'maximum character is 13';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: size.height * 0.02),
+
+                /// Password
+                Obx(
+                  () => TextFormField(
+                    style: kTextFormFieldStyle(),
+                    controller: passwordController,
+                    obscureText: simpleUIController.isObscure.value,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.lock_open),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          simpleUIController.isObscure.value
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          simpleUIController.isObscureActive();
+                        },
+                      ),
+                      hintText: 'Password',
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      } else if (value.length < 7) {
+                        return 'at least enter 6 characters';
+                      } else if (value.length > 13) {
+                        return 'maximum character is 13';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(height: size.height * 0.01),
+                Text(
+                  'Creating an account means you\'re okay with our Terms of Services and our Privacy Policy',
+                  style: kLoginTermsAndPrivacyStyle(size),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: size.height * 0.02),
+
+                /// Login Button
+                loginButton(),
+                SizedBox(height: size.height * 0.03),
+
+                /// Navigate To Sign Up Screen
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                    namaController.clear();
+                    emailController.clear();
+                    passwordController.clear();
+                    _formKey.currentState?.reset();
+                    simpleUIController.isObscure.value = true;
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Don\'t have an account?',
+                      style: kHaveAnAccountStyle(size),
+                      children: [
+                        TextSpan(
+                          text: ' Sign up',
+                          style: kLoginOrSignUpTextStyle(size),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  // Login Button
+  Widget loginButton() {
+    return SizedBox(
+      width: double.infinity,
+      height: 55,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor:
+              MaterialStateProperty.all(Colors.deepPurpleAccent),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+        onPressed: () {
+          if (_formKey.currentState!.validate()) {
+            // TODO: Navigate To your Home Page
+          }
+        },
+        child: const Text('Login'),
+      ),
+    );
   }
 }
